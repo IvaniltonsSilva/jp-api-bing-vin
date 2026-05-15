b7dfaeb3ac373ca5bbcb5578448d22def2b545e5
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-15 20:06:30 -0300

    chore: update shareCode to H4ICCJ86231

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=3NAS17RJ28O&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=H4ICCJ86231&afp=tapib' });
     res.end();
   }
