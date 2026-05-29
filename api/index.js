fc322d1417b06f472a738e7ae8a909ee80f5b95d
JCPrime01 <vanzokiller@gmail.com>
2026-05-29 20:07:25 -0300

    chore: shareCode 9YLY6ETXNQ0 -> CHIWPA2OD5D (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=9YLY6ETXNQ0&afp=td' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=CHIWPA2OD5D&afp=td' });
     res.end();
   }
